a98d54afe3ca8aaaa3e846baf69c8bd9ce014917
Samuel Solano <sdsolano@uninorte.edu.co>
2024-12-30 11:57:33 -0500

    .

--- variacion.py
+++ variacion.py
@@ -46,3 +46,41 @@ for fenomeno in ['NORMAL', 'NIÑO', 'NIÑA']:
     print(f"\nEstadísticas para {fenomeno}:")
     print(f"Número de meses: {len(data)}")
     print(f"Demanda promedio: {data['total'].mean():,.0f}")
+
+    # los resultados de este codigo son:
+    
+    # Variaciones por mes:
+    #     Mes  Variacion_Nino_%  Variacion_Nina_% 
+    # 0     1              2.63              0.59
+    # 1     2              4.04              0.96
+    # 2     3              0.58              0.56
+    # 3     4               NaN             -3.06
+    # 4     5               NaN               NaN
+    # 5     6              0.71               NaN
+    # 6     7              0.92               NaN
+    # 7     8             -0.28               NaN
+    # 8     9              1.30               NaN
+    # 9    10              0.63             -1.53
+    # 10   11             -0.40              0.01
+    # 11   12              0.25             -1.30
+
+    # Promedio general Niño: 1.04%
+    # Promedio general Niña: -0.54%
+
+    # Estadísticas para NORMAL:
+    # Número de meses: 77
+    # Demanda promedio: 4,284,473,039
+
+    # Estadísticas para NIÑO:
+    # Número de meses: 20
+    # Demanda promedio: 4,753,942,167
+
+    # Estadísticas para NIÑA:
+    # Número de meses: 11
+    # Demanda promedio: 3,174,750,852
+
+    # Por tanto los factores serán:
+
+    # Para el fenómeno del Niño: 1.0104 
+    # Para el fenómeno de la Niña: 0.9946
+
